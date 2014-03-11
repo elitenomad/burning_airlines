4e96232bdbe8a3604e911429ac2c8b5d911b8894
elitenomad <stalin.pranava@gmail.com>
2014-03-11 22:29:39 +1100

    Devise setup Rspec gem addition boot strap gem addition Setting up basic layout for the project

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ BurningAirlines::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Added due to devise configuration
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
