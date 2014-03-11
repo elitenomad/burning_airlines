e9378fc8bae06f157cb3c40b2e3b17e4fda41009
elitenomad <stalin.pranava@gmail.com>
2014-03-11 17:21:37 +1100

    Initializing Burning Lines repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BurningAirlines::Application.config.secret_key_base = 'eacce32f1b1a66849455507747e91de4139e8b479678d6b9a0a19f57326dd53ecd97c52a006c3819cfa12c8f12880c8b3c51cba6eacab3a959a18e22f89be2e0'
