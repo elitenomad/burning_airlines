bf03c357c7c0dfcae7ba6cea34a4cdc789ebab17
elitenomad <stalin.pranava@gmail.com>
2014-03-11 23:16:23 +1100

    Validations for airplan and Flight models

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,9 @@ class Flight < ActiveRecord::Base
   belongs_to :airplane
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations, source: :user
+
+  validates :flight_no, presence: true, uniqueness: true
+  validates :origin, presence: true
+  validates :destination, presence: true
+  validates :start_time, presence: true
 end
